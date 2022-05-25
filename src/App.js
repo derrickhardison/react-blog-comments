df77f9ba97f08c3f04742beb5064684e2b26fbeb
dlhardi <derricklhardison@gmail.com>
2022-05-25 01:21:44 -0400

    created ApprovalCard component and added CommentDetail comp as children

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import faker from "faker";
 import CommentDetail from "./components/CommentDetail";
+import ApprovalCard from "./components/ApprovalCard";
+import "./style/App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="ui container comments">
-        <CommentDetail
-          author="Sam"
-          date="Today at 11:00AM"
-          text="Nice blog post!"
-          photo={faker.image.image()}
-        />
-        <CommentDetail
-          author="Alex"
-          date="Today at 3:00PM"
-          text="I totally agree!"
-          photo={faker.image.image()}
-        />
-        <CommentDetail
-          author="Jane"
-          date="Today at 8:00PM"
-          text="Thanks for sharing!"
-          photo={faker.image.image()}
-        />
+        <ApprovalCard>
+          <CommentDetail
+            author="Sam"
+            date="Today at 11:00AM"
+            text="Nice blog post!"
+            photo={faker.image.image()}
+          />
+        </ApprovalCard>
+
+        <ApprovalCard>
+          <CommentDetail
+            author="Alex"
+            date="Today at 3:00PM"
+            text="I totally agree!"
+            photo={faker.image.image()}
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail
+            author="Jane"
+            date="Today at 8:00PM"
+            text="Thanks for sharing!"
+            photo={faker.image.image()}
+          />
+        </ApprovalCard>
       </div>
     </div>
   );
